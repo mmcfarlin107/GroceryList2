58e9f88470d5bbe785d48f06e656537ff1ca2207
mmcfarlin107 <mcfarlin107@gmail.com>
2017-08-31 18:50:41 -0400

    Delete is working.  Working on displaying adding and subtracting to Total.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
+var total;
 
-  
+// When the Add item button is clicked, this function creates a new table row with the data the user input and adds the new amount into the price total
 $('#add').click(function(){
   console.log("clicked");
   var groceryInput = $('#groceryInput').val();
@@ -12,13 +13,20 @@ $('#add').click(function(){
     "</td> <td>" + amountInput +
     "</td><td> $" + priceInput +
     "</td><td> $" + itemTotal.toFixed(2) +
-    "</td> <td> <button type='button'> Delete Item </button> </td> </tr>");
+    "</td> <td> <button class='deleteThis'> Delete Item </button> </td> </tr>");
+
+    total += itemTotal;
+
+    $('#totalAmount').append("<h3> Total: $" + itemTotal.toFixed(2) + "</h3>")
 
 $('#groceryInput').val('');
 $('#amountInput').val('');
 $('#priceInput').val('');
+});
 
-
+//Removes Table Row on Delete
+$("#listTable").on('click', '.deleteThis', function () {
+    $(this).closest('tr').remove();
 });
 
 
