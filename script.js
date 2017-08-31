37e8372f09685df91874027ec03a828fbf05eabe
mmcfarlin107 <mcfarlin107@gmail.com>
2017-08-31 16:27:14 -0400

    Added an image and the delete button

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
+
+  
 $('#add').click(function(){
   console.log("clicked");
   var groceryInput = $('#groceryInput').val();
   var amountInput = $('#amountInput').val();
   var priceInput = $('#priceInput').val();
   var itemTotal = amountInput * priceInput;
+
   $('#groceryItems').append("<tr><td>" + groceryInput +
     "</td> <td>" + amountInput +
-    "</td><td>" + priceInput +
-    "</td><td>" + itemTotal + "</td></tr>");
+    "</td><td> $" + priceInput +
+    "</td><td> $" + itemTotal.toFixed(2) +
+    "</td> <td> <button type='button'> Delete Item </button> </td> </tr>");
+
+$('#groceryInput').val('');
+$('#amountInput').val('');
+$('#priceInput').val('');
+
+
 });
 
 
